0dab81ca110276149511f0efca862a3762b81add
ehtishamgeeksquad <development.geeksquad@gmail.com>
2020-01-23 17:19:56 +0500

    dashboard color variable add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import 'bulma/css/bulma.css'
 import './assets/Style/Common.scss'
 import 'bulma-helpers/css/bulma-helpers.min.css'
-import View from './Components/User/ViewUser/viewUser'
+import View from './Components/Dashboard/dashboard'
 
 
 export default () => {
