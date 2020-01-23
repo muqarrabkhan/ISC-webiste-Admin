f5daab89f0d32b69a489d01841ccacbae26b4d19
akashahmad <engr.akashahmad@gmail.com>
2020-01-23 21:53:37 +0500

    routes added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react'
-import { BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import 'bulma/css/bulma.css'
 import './assets/Style/Common.scss'
 import 'bulma-helpers/css/bulma-helpers.min.css'
 import Route from './Routes'
 
 
-
-
-
 export default () => {
-  return (
-    <>
-      <BrowserRouter>
+    return (
         <Route/>
-      </BrowserRouter>
-    </>
-  );
+    );
 }
 
--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import {Route, Switch } from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Sidenav from './Components/Sidenav/sidenav'
 import Administrator from './Components/Administrator/ViewAdministrators/viewAdministrator'
 
 export default () => {
     return (
-        <div className="main-container">
-            <Sidenav>
-                <div className="main-routes">
-                    <div className="right-section">
+        <div className="main-container is-flex">
+            <Sidenav/>
+            <div className="main-routes">
+                <div className="right-section">
+                    <BrowserRouter>
                         <Switch>
-                            <Route path={"/administrator"} component={Administrator} />
+                            <Route path={"/administrator"} component={Administrator}/>
                         </Switch>
-                    </div>
+                    </BrowserRouter>
                 </div>
-            </Sidenav>
+            </div>
         </div>
     );
 }
